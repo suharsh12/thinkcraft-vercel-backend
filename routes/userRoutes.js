e0a6cbd4e4e45b3d533213c46f20ec4424e098b5
suharsh12 <85607686+suharsh12@users.noreply.github.com>
2025-08-09 21:49:23 +0530

    Update userRoutes.js
    changee again

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserCreations, toggleLikeCreation } from '../controllers/usercontroller.js';
+import { getUserCreations, toggleLikeCreation } from '../controllers/userController.js';
 import { auth } from '../middlewares/auth.js';
 
 export const userRouter = express.Router();
