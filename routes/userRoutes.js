4dc658319178203b8350fabdd5074e6f31bd891c
suharsh <suharshj12@gmail.com>
2025-08-09 18:58:52 +0530

    adding backend to git

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getUserCreations, toggleLikeCreation } from '../controllers/userController.js';
+import { auth } from '../middlewares/auth.js';
+
+export const userRouter = express.Router();
+
+userRouter.get('/get-user-creations', auth, getUserCreations)
+userRouter.post('/toggle-like-creation', auth, toggleLikeCreation)
+
+export default userRouter;
